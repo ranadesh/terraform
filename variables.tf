e94f8231c6a74bab4314c4ca69954ea066e33ca5
ranadesh <ranadeshmukh23@gmail.com>
2025-02-23 11:52:57 +0000

    Your commit message here

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "aws_region" {
   description = "The AWS region to deploy resources"
-  default     = "ap-south-1"
+  default     = "ap-northeast-3"
 }
 variable "ami" {
   description ="The AMI to use for the EC2 instance"
-  default     = "ami-0d682f26195e9ec0f"
+  default     = "ami-0439cd8bc5628c9e8"
 }
 
 variable "instance_type" {
